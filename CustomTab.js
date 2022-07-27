702ad11a9d9b8c903916d19f5b056ddc8523336a
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-07-27 22:38:39 +0530

    Update CustomTab.js

--- CustomTab.js
+++ CustomTab.js
@@ -55,3 +55,74 @@ export const useAxiosFetch = (url) => {
   }, []);
   return { data, error, loading };
 };
+
+
+// use it
+
+import React, { useState, useEffect } from "react";
+import { useAxiosFetch } from "../custom-hooks/useAxiosFetch";
+const TutorialsList = () => {
+  const [tutorials, setTutorials] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const { fetchData, data, loading, error } = useAxiosFetch({
+    method: "GET",
+    url: "/tutorials",
+    params: {
+      title: searchTitle,
+    },
+  });
+  useEffect(() => {
+    if (data) {
+      setTutorials(data);
+      console.log(data);
+    } else {
+      setTutorials([]);
+    }
+  }, [data]);
+  useEffect(() => {
+    if (error) {
+      console.log(error);
+    }
+  }, [error]);
+  useEffect(() => {
+    if (loading) {
+      console.log("retrieving tutorials...");
+    }
+  }, [loading]);
+  const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+  };
+  const findByTitle = () => {
+    fetchData();
+  };
+  // ...
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by title"
+          value={searchTitle}
+          onChange={onChangeSearchTitle}
+        />
+        <button type="button" onClick={findByTitle} >
+          Search
+        </button>
+      </div>
+      <div>
+        <h4>Tutorials List</h4>
+        {loading && <p>loading...</p>}
+        <ul className="list-group">
+          {tutorials &&
+            tutorials.map((tutorial, index) => (
+              <li key={index} >
+                {tutorial.title}
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default TutorialsList;
